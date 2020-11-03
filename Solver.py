ed515399350700a946c5daa86b27f11090b6c4db
Wan-Huang Yang <wyang47@jhu.edu>
2020-11-02 19:36:20 -0500

    doc Lazor and Block

--- Solver.py
+++ Solver.py
@@ -2,62 +2,193 @@ from BFF import BFF
 
 
 class Lazor:
+    '''
+    The class to represent a moving lazor
+    '''
+
     def __init__(self, x, y, dx, dy):
+        '''
+        The class initializer
+
+        **Parameters**
+
+            x: *int*
+                x of the lazor's position
+            y: *int*
+                y of the lazor's position
+            dx: *int*
+                x of the lazor's diration
+            dy: *int*
+                y of the lazor's diration
+
+        '''
         self.x = x
         self.y = y
         self.dx = dx
         self.dy = dy
 
     def __eq__(self, other):
+
+        '''
+        eq operator for this class, can be used in a set
+
+        **Parameters**
+
+            other: *Lazor class*
+                comparing lazor
+
+        **Returns**
+
+            flag: *bool*
+                If self euqals to other
+
+        '''
         return other and self.x == other.x and self.y == other.y \
             and self.dx == other.dx and self.dy == other.dy
 
     def __ne__(self, other):
+        '''
+        ne operator for this class, can be used in a set
+
+        **Parameters**
+
+            other: *Lazor class*
+                comparing lazor
+
+        **Returns**
+
+            flag: *bool*
+                If self does not euqal to other
+
+        '''
         return not self.__eq__(other)
 
     def __hash__(self):
+        '''
+        hash value for this class, can be used in a set
+
+        **Returns**
+
+            hash: *int*
+                The hash value for the lazor
+
+        '''
         return hash((self.x, self.y, self.dx, self.dy))
 
     def hit_block(self, board):
-        if self.x % 2 == 1:
+        '''
+        The block that this lazor is going to hit
+
+        **Parameters**
+
+            board: *2-D list of Block class*
+                The board
+
+        **Returns**
+
+            block: *Block class or None*
+                The hit block
+
+        '''
+        # calculate the hit block's coordinates
+        if self.x % 2 == 1:  # hit horizontally
             mx = (self.x - 1)//2
             my = self.y//2 - (1 if self.dy == -1 else 0)
-        else:
+        else:  # hit vertically
             mx = self.x//2 - (1 if self.dx == -1 else 0)
             my = (self.y - 1)//2
 
+        # return None if the coordinates are outside of the board
         if mx < 0 or my < 0 or mx >= len(board[0]) or my >= len(board):
             return None
         return board[my][mx]
 
     def step(self):
+        '''
+        Return the lazor where self lazor moves straightforward
+
+        **Returns**
+
+            lazor: *Lazor class*
+                The lazor where self lazor moves straightforward
+
+        '''
         return Lazor(self.x + self.dx, self.y + self.dy, self.dx, self.dy)
 
     def reflect(s):
-        if s.x % 2 == 1:
+        '''
+        Return the lazor where self lazor reflects
+
+        **Returns**
+
+            lazor: *Lazor class*
+                The lazor where self lazor reflects
+
+        '''
+        if s.x % 2 == 1:  # reflect horizontally
             return Lazor(s.x + s.dx, s.y - s.dy, s.dx, -s.dy)
-        else:
+        else:  # reflect vertically
             return Lazor(s.x - s.dx, s.y + s.dy, -s.dx, s.dy)
 
 
 class Block:
-    blockMap = ["A", "B", "C"]
+    '''
+    The class to represent a block
+    '''
 
     def __init__(self, type):
+        '''
+        The class initializer
+
+        **Parameters**
+
+            type: *str*
+                the type of block
+
+        '''
         self.type = type
 
     def placable(self):
+        '''
+        Return if the block can be placed something
+
+        **Returns**
+
+            flag: *bool*
+                Indicate if the block can be placed something
+
+        '''
         return self.type == "o"
 
     def place(self, type):
+        '''
+        Replace self's type with te type parameter
+
+        **Parameters**
+
+            type: *str*
+                the type of block
+
+        '''
         self.type = type
 
     def react(self, lazor, dfs):
-        if self.type == "o" or self.type == "x":
+        '''
+        Call dfs to react what will happen according to the lazor and the block
+
+        **Parameters**
+
+            lazor: *Lazor class*
+                The moving lazor
+            dfs: *function*
+                DFS function
+
+        '''
+        if self.type == "o" or self.type == "x":  # just go through
             dfs(lazor.step())
-        elif self.type == "A":
+        elif self.type == "A":  # reflect
             dfs(lazor.reflect())
-        elif self.type == "C":
+        elif self.type == "C":  # go through and reflect
             dfs(lazor.step())
             dfs(lazor.reflect())
 
@@ -69,6 +200,8 @@ class Solver:
         self.lazors = [Lazor(x, y, dx, dy) for x, y, dx, dy in bff.lazors]
 
     def solve(self):
+        blockMap = ["A", "B", "C"]
+
         blocks = self.bff.blocks
         board = self.board
         h = len(board)
@@ -85,7 +218,7 @@ class Solver:
                         continue
                     if not board[x][y].placable():
                         continue
-                    board[x][y].place(Block.blockMap[b])
+                    board[x][y].place(blockMap[b])
 
                     if dfs(b, x * w + y):
                         return True
